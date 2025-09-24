961050080c27f972bf8d5359287a5afa319e44d3
doyeon <cumber88@naver.com>
2025-09-24 15:57:30 +0900

    성지 클레임 붙이기 허용

--- Towny/src/main/java/com/palmergames/bukkit/towny/object/TownyWorld.java
+++ Towny/src/main/java/com/palmergames/bukkit/towny/object/TownyWorld.java
@@ -913,7 +913,7 @@ public class TownyWorld extends TownyObject {
 
 		double minSqr = -1;
 		for (Town town : getTowns().values()) {
-			if (town.getNationOrNull() != null && town.getNationOrNull().getName().contains("정기선")){
+			if (town.getNationOrNull() != null && (town.getNationOrNull().getName().contains("정기선") || town.getNationOrNull().getName().contains("성지"))){
 				continue;
 			}
 			if (isNpcCheck) {
