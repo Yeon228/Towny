07a1440894aac28499b72922e3b73944fc366fce
doyeon <cumber88@naver.com>
2026-04-22 00:57:42 +0900

    액자 스위치 변경

--- Towny/src/main/java/com/palmergames/bukkit/util/EntityLists.java
+++ Towny/src/main/java/com/palmergames/bukkit/util/EntityLists.java
@@ -27,9 +27,9 @@ public class EntityLists extends AbstractRegistryList<EntityType> {
 	
 	public static final EntityLists DYEABLE = newBuilder().add("sheep", "wolf", "cat").build();
 	
-	public static final EntityLists SWITCH_PROTECTED = newBuilder().add("chest_minecart", "furnace_minecart", "hopper_minecart", "chest_boat").build();
+	public static final EntityLists SWITCH_PROTECTED = newBuilder().add("chest_minecart", "furnace_minecart", "hopper_minecart", "chest_boat", "item_frame", "glow_item_frame").build();
 	
-	public static final EntityLists RIGHT_CLICK_PROTECTED = newBuilder().add("tropical_fish", "salmon", "cod", "item_frame", "glow_item_frame", "painting", "leash_knot", "command_block_minecart", "tnt_minecart", "spawner_minecart", "tadpole", "axolotl").build();
+	public static final EntityLists RIGHT_CLICK_PROTECTED = newBuilder().add("tropical_fish", "salmon", "cod", "painting", "leash_knot", "command_block_minecart", "tnt_minecart", "spawner_minecart", "tadpole", "axolotl").build();
 	
 	public static final EntityLists DESTROY_PROTECTED = newBuilder().add("item_frame", "glow_item_frame", "painting", "armor_stand", "end_crystal", "minecart", "chest_minecart", "command_block_minecart", "hopper_minecart").build();
 	
